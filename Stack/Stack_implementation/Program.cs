c30f7dc693bc20b6191573e9c39724508cb5c01d
Fayyad <mohamedfayad080@gmail.com>
2024-02-01 17:36:30 +0200

    Add Stack directory

---
+++ Stack/Stack_implementation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Stack_implementation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack_Array_based<int> obj = new(10);
+        obj.Push(10);
+        obj.Push(24);
+        obj.Push(25);
+        obj.Push(70);
+        obj.Push(19);
+        obj.PrintStack();
+        
+    }
+}
---
+++ Stack/Stack_implementation/Stack_Array_based.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Cryptography;
+
+namespace Stack_implementation;
+
+//<T> represent to Generic Stack that can work with any data type.
+public class Stack_Array_based<T>(int size)
+{
+    public int Top { get; set; } = -1;
+    public T[] Itmes { get; set; } = new T[size];
+
+    public void Push(T value)
+    {
+
+        if (Top == size - 1)
+        {
+            Console.WriteLine("Stack Overflow");
+        }
+        else
+        {
+            Top++;
+            Itmes[Top] = value;
+        }
+    }
+
+    public void PrintStack()
+    {
+        if (IsEmpty())
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.Write("[ ");
+            for (int i = Top; i >= 0; i--)
+            {
+                Console.Write($"{Itmes[i]} ");
+            }
+            Console.Write("]");
+        }
+    }
+
+    public bool IsEmpty()
+    {
+        return Top < 0;
+    }
+    
+    public void Pop()
+    {
+        if (IsEmpty())
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Top--;
+        }
+    }
+    public int Peek()
+    {
+        if (IsEmpty())
+        {
+            Console.WriteLine("Stack is empty");
+            return -1;
+        }
+        else
+        {
+            return Top;
+        }
+    }
+}
